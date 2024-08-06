93c56c11e2bd25db81c0748686c71df5fb4acf4f
Joyce Chidiadi <jcchidiadi@gmail.com>
2024-08-06 07:25:15 -0600

    Make a unique password generator

--- python-projects/Day5/password_generator.py
+++ python-projects/Day5/password_generator.py
@@ -17,16 +17,37 @@ pwd_letters = int(input("How many letters do you want in your password: \n"))
 pwd_nums = int(input("How many numbers do you want: \n"))
 pwd_symbols = int(input(f"How many symbols: \n"))
 
-password = ""
+# password = ""
+# for char in range(0, pwd_letters):
+#     password += random.choice(letters)
+
+# for char in range(0, pwd_nums):
+#     password += random.choice(numbers)
+
+# for char in range(0, pwd_symbols):
+#     password += random.choice(symbols)
+
+# print(f"Here is your unique password:\n {password}")
+
+###################### Using Shuffle library ######################
+# Reshuffle the password output. Add the new passwords to a list instead of a string
+password_list = []
+
 for char in range(0, pwd_letters):
-    password += random.choice(letters)
+    password_list += random.choice(letters)
 
 for char in range(0, pwd_nums):
-    password += random.choice(numbers)
+    password_list += random.choice(numbers)
 
 for char in range(0, pwd_symbols):
-    password += random.choice(symbols)
+    password_list += random.choice(symbols)
 
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+# Then turn the reshuffed password to a string
+password = ""
+for char in password_list:
+    password += char
 print(f"Here is your unique password:\n {password}")
-
-# Reshuffle the password output. Add the new passwords to a list instead of a string
