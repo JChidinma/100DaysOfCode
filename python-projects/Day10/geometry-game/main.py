20b770d112162974fe30c0f2607e17928bc6a72a
Joyce Chidiadi <jcchidiadi@gmail.com>
2024-08-08 16:47:46 -0600

    Geometry and painting a house

---
+++ python-projects/Day10/geometry-game/main.py
@@ -0,0 +1,16 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+# Go to a certain cordinate
+my_turtle.goto(50, 75)
+
+my_turtle.forward(100)  # Move 100 pixels
+my_turtle.left(90)  # Turn 90 degrees left
+my_turtle.forward(200)
+my_turtle.left(90)
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(200)
+
+turtle.done()
