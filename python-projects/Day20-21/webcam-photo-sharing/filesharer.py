2a62232a0d19fc254c2bd82a63d6bcb98d8df0d3
Joyce Chidiadi <jcchidiadi@gmail.com>
2024-08-19 19:18:31 -0600

    Build a Webcam photo sharing app

---
+++ python-projects/Day20-21/webcam-photo-sharing/filesharer.py
@@ -0,0 +1,9 @@
+class FileSharer:
+    def __init__(self, filepath, api_key="jakfkjakaf"):
+        self.filepath = filepath
+        self.api_key = api_key
+
+    def share(self):
+        client = Client(self.api_key)
+        new_file_link = client.upload(filepath=self.filepath)
+        return new_file_link.url
---
+++ python-projects/Day20-21/webcam-photo-sharing/main.py
@@ -0,0 +1,40 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from filesharer import FileSharer
+
+Builder.load_file('frontend.kv')
+
+# This class can be used for the Webcam class
+
+
+class CameraScreen(Screen):
+    def start(self):
+        self.ids.camera.play = True
+        self.ids.camera_button.text = "Stop Camera"
+        self.ids.camera.texture = self.ids.camera._camera.texture
+
+    def stop(self):
+        self.ids.camera.play = False
+        self.ids.camera_button.text = "Start Camera"
+        self.ids.camera.texture = None
+
+    def capture(self):
+        pass
+
+
+class ImageScreen(Screen):
+    pass
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+
+MainApp().run()
