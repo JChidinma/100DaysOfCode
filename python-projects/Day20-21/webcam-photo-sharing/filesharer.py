5fcd1103e2ea374b8219fe61f113c0e5ef142e93
Joyce Chidiadi <jcchidiadi@gmail.com>
2024-08-23 09:32:01 -0600

    Update Camera Action

--- python-projects/Day20-21/webcam-photo-sharing/filesharer.py
+++ python-projects/Day20-21/webcam-photo-sharing/filesharer.py
@@ -1,5 +1,13 @@
+from filestack import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.environ['FILESTACK_API_KEY']
+
+
 class FileSharer:
-    def __init__(self, filepath, api_key="jakfkjakaf"):
+    def __init__(self, filepath, api_key=API_KEY):
         self.filepath = filepath
         self.api_key = api_key
 
