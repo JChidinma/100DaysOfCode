d77fc66e45cb573d7da11c580f8d7181aaffb507
Joyce Chidiadi <jcchidiadi@gmail.com>
2024-09-15 14:01:18 -0600

    Automate news email

---
+++ python-projects/Day40/main.py
@@ -0,0 +1,35 @@
+import yagmail
+import pandas as pd
+import datetime
+from dotenv import load_dotenv
+from news import NewsFeed
+load_dotenv(override=True)
+
+df = pd.read_excel('people.xlsx')
+gmail_user = os.environ['gmail_user']
+gmail_password = os.environ['gmail_app_password']
+
+for index, row in df.iterrows():
+    # print(row)
+    # print(row['name'])
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+    yesterday = (datetime.datetime.now() -
+                 datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+
+    news_feed = NewsFeed(
+        interest=row['interest'], from_date=yesterday, to_date=today, language='en')
+    email = yagmail.SMTP(user=gmail_user,
+                         password=gmail_password)
+    email.send(
+        to=row['email'],
+        subject=f"Your {row['interest']} news report for today!",
+        contents=f"Hi {row['name']},\nSee what's up about {row['interest']} today. \n\n{news_feed.get()} \n\nRegards,\nJoyce"
+        # attachments="100DaysOfCode/python-projects/Day40/automate_emails/design.txt"
+    )
+    # email.send(
+    #     to="dichidiadi@gmail.com",
+    #     subject="Mummy is Testing Her Code",
+    #     contents="I am testing a code that gets stories and news of your interest and sends you an email. \n\nRegards,\nJoyce",
+    #     attachments="100DaysOfCode/python-projects/Day40/automate_emails/design.txt"
+    # )
+# print(df)
