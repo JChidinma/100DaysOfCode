ab5f9051d22d96e3a3f4eb049ed7d24fbbcc2399
Joyce Chidiadi <jcchidiadi@gmail.com>
2024-10-04 13:54:37 -0600

    Make a simple calculator

---
+++ python-projects/Day63/calculator.py
@@ -0,0 +1,79 @@
+calc = """
+Simple Calculator
+ _____________________
+|  _________________  |
+| | JO  3.141592654 | |
+| |_________________| |
+|  __ __ __ __ __ __  |
+| |__|__|__|__|__|__| |
+| |__|__|__|__|__|__| |
+| |__|__|__|__|__|__| |
+| |__|__|__|__|__|__| |
+| |__|__|__|__|__|__| |
+| |__|__|__|__|__|__| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|
+"""
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+# Add the operations into a function
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+
+def calculator():
+    print(calc)
+
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(
+            f"Type 'y' to continue calculating with {answer}, or type 'n' to start over: ")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print('\n' * 20)
+            # break
+            calculator()
+
+
+calculator()
