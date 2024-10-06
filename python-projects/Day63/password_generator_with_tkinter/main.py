76b19df9495aa72ac2aa716e6b20e8d3d3bf572b
Joyce Chidiadi <jcchidiadi@gmail.com>
2024-10-06 16:06:18 -0600

    password manager with clipboard clipper

--- python-projects/Day63/password_generator_with_tkinter/main.py
+++ python-projects/Day63/password_generator_with_tkinter/main.py
@@ -1,18 +1,113 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 # --------------------------PASSWORD GENERATOR ------------------------
 
-# ------------------------ SAVE PASSWORD -----------------------
+
+def generate_password():
+    symbols = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '+', ';', ':', '?']
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+               'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    pwd_letters = randint(8, 10)
+    pwd_nums = randint(2, 4)
+    pwd_symbols = randint(2, 4)
+
+    # use list comprehension to replace the for loops
+    password_letters = [choice(letters) for _ in range(pwd_letters)]
+    password_nums = [choice(numbers) for _ in range(pwd_nums)]
+    password_symbols = [choice(symbols) for _ in range(pwd_symbols)]
+    # for char in range(pwd_letters):
+    #     password_list.append(random.choice(letters))
+
+    # for char in range(pwd_nums):
+    #     password_list.append(random.choice(numbers))
+
+    # for char in range(pwd_symbols):
+    #     password_list.append(random.choice(symbols))
+
+    password_list = password_letters + password_symbols + password_nums
+    shuffle(password_list)
+
+    # Then turn the reshuffed password to a string
+    # password = ""
+    # for char in password_list:
+    #     password += char
+    # print(f"Here is your unique password:\n {password}")
+    password = " ".join(password_list)
+    # print(f"Here is your unique password:\n {password}")
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+    # ------------------------ SAVE PASSWORD -----------------------
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(
+            title="Oops!", message="Please make sure you haven't left any fields blank.")
+    else:
+        is_ok = messagebox.askokcancel(
+            title=website,
+            message=f"These are the details you entered: \nEmail: {email} "
+            f"\nPassword: {password}\nIs it OK to save?"
+        )
+
+        if is_ok:
+            with open('password_manager.txt', 'a') as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ------------------------ UI SETUP -----------------------------------
-
 window = Tk()
 window.title("Password Generator")
-window.config(padx=2, pady=20)
+window.config(padx=20, pady=20)
 
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
+# canvas.pack()
+
+# Labels
+website_label = Label(text="Website: ")
+website_label.grid(row=1, column=0)
+
+
+email_label = Label(text="Email/Username: ")
+email_label.grid(row=2, column=0)
+
+password_label = Label(text="Password: ")
+password_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, 'alice.you@gmail.com')
+
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+# Button
+generate_password_button = Button(
+    text="Generate Password", command=generate_password())
+generate_password_button.grid(row=3, column=2)
+
+add_button_to_manager = Button(text="Add", width=36, command=save)
+add_button_to_manager.grid(row=4, column=1, columnspan=2)
+
 window.mainloop()
+save()
